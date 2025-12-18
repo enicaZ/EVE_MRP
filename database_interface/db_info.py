51c334e1fb8476887ea0a5f95f9347a91b85e3be
enicaz <111721320@qq.com>
2025-12-18 22:51:28 +0800

    new

--- database_interface/db_info.py
+++ database_interface/db_info.py
@@ -1,5 +1,8 @@
 import sqlite3
 
 def db_path():
+    """
+    数据库路径函数
+    """
     pass
 
--- database_interface/db_process.py
+++ database_interface/db_process.py
@@ -1,7 +1,11 @@
 import sqlite3
+from .db_info import *
+
 
 def load_sso_info():
+    """加载SSO的信息，包括client_id、client_secret、redirect_uri等"""
     pass
 
 def save_sso_info():
+    """保存SSO的信息，包括client_id、client_secret、redirect_uri等"""
     pass
