0a380a418160663bc5fbffff13b71a631d7d8d12
enica_office <11721320@qq.com>
2025-12-19 17:55:40 +0800

    office

--- test.py
+++ test.py
@@ -1,3 +1,11 @@
-import sqlite3
+# test_pyside6.py
+import sys
+from PySide6.QtWidgets import QApplication, QLabel
+from PySide6.QtCore import Qt
 
-#...........
+app = QApplication(sys.argv)
+label = QLabel("Hello PySide6!")
+label.setAlignment(Qt.AlignCenter)
+label.resize(400, 300)
+label.show()
+sys.exit(app.exec())
